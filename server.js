d268b9409964e5aab96283ad2e4c8a81885b11d9
ruksaar <rukkuabc786@gmail.com>
2017-03-18 16:10:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one', function (req, res) { 
+    res.sendFile('article one will be requested and will be served here');
+});
+
+app.get('article-two', function (req, res) { 
+    res.sendFile('article two will be requested and will be served here');
+});
+
+app.get('article-three', function (req, res) { 
+    res.sendFile('article three will be requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
